960393f78c877ae4f5b72107a55e3470b16b2212
Michel Dejoux <michouzm@hotmail.com>
2021-06-06 23:41:59 +0200

    first version: displays next departures

--- MMM-Navitia.js
+++ MMM-Navitia.js
@@ -8,28 +8,48 @@ Module.register('MMM-Navitia', {
         limit               :4,
         departures          :[],
         schedules           :[],
-        refreshPeriod       :3 * 1000 // every 120s (in ms)
+        refreshPeriod       :120 * 1000 // every 120s (in ms)
 	},
 
     
     start: function() {
-        // for (var i=0; i<config.departures.length; i++)
-        // {
-        //     schedules.push
-        //     (
-        //         {
-        //             departureStopPoint  :config.departures.departureStopPoint,
-        //             departureLine       :config.departures.departureLine,
-        //             direction           :config.departures.direction,
-        //             nextDepartures      :[],
-        //             mode                :"Train",
-        //         }
-        //     )
-        // };
+        for (var i = 0; i < this.config.departures.length; i++)
+        {
+            this.defaults.schedules.push
+            (
+                {
+                    config:
+                    {
+		                navitiaURL              :"https://api.navitia.io/v1/coverage/fr-ne",
+                        navitiaApiKey           : this.config.navitiaApiKey,
+                        limit                   : this.config.departures[i].limit
+                    },
+                    request:
+                    {
+                        departureStopPoint      : this.config.departures[i].departureStopPoint,
+                        departureStopPointName  : '',
+                        departureLine           : this.config.departures[i].departureLine,
+                        direction               : this.config.departures[i].direction,
+                        mode                    : "Train",
+                    },
+                    results                     : [ // Array of :
+                    // {
+                    //     headSign,
+                    //     lineCode,
+                    //     lineColor,
+                    //     departureTime,
+                    //     direction,
+                    //     departureStopPointName
+                    // }
+                    ],
+                }
+            )
+        };
 
-        // setInterval(() => {
-            this.sendSocketNotification('MMM-Navitia--INIT', this.config);
-        //   }, this.refreshPeriod);
+        this.sendSocketNotification('MMM-Navitia--Departures', this.defaults.schedules);
+        setInterval(() => {
+            this.sendSocketNotification('MMM-Navitia--Departures', this.defaults.schedules);
+          }, this.defaults.refreshPeriod);
 
         Log.log(this.name + ' is started!');
     },
@@ -40,35 +60,43 @@ Module.register('MMM-Navitia', {
 		var wrapper = document.createElement("div");
         wrapper.classList.add("mmm-navitia");
 
-        if (!this.departures)
-            return wrapper;
-        
-        for (var i = 0; i < this.departures.length; i++)
+        // if (!this.defaults.schedules)
+        // {
+        //     var departureTitle = document.createElement("header");
+        //     departureTitle.classList.add("module-header");
+        //     departureTitle.innerHTML = 'Prochains départs - Loading...';
+        //     return wrapper;
+        // }
+
+        for (var i = 0; i < this.defaults.schedules.length; i++)
         {
-            var departureTitle = document.createElement("header");
-            departureTitle.classList.add("module-header");
-            departureTitle.innerHTML = 'Prochains départs - ' + this.departures[i].departureStopPoint;
-            wrapper.appendChild(departureTitle);
-
             var table = document.createElement("table");
-            table.className = this.config.tableClass;
+            table.classList.add("schedules");
 
-            for (var j = 0; j < this.departures[i].nextTrains.length; j++)
+            for (var j = 0; j < this.defaults.schedules[i].results.length; j++)
             {
+                if (j==0)
+                {
+                    var departureTitle = document.createElement("header");
+                    departureTitle.classList.add("module-header");
+                    departureTitle.innerHTML = 'Prochains départs - ' + this.defaults.schedules[i].results[j].departureStopPointName;
+                    wrapper.appendChild(departureTitle);
+                }
+
                 var row = document.createElement("tr");
                 table.appendChild(row);
             
                 var lineCell = document.createElement("td");
                 var lineSpan = document.createElement("span");
                 lineSpan.className = "line";
-                lineSpan.setAttribute('style',"border-color: #"+this.departures[i].nextTrains[j].lineColor+"; color: #"+this.departures[i].nextTrains[j].lineColor+";")
-                lineSpan.setAttribute('data-line', this.departures[i].nextTrains[j].lineColor);
-                lineSpan.innerHTML = this.departures[i].nextTrains[j].lineCode;
+                lineSpan.setAttribute('style',"border-color: #"+this.defaults.schedules[i].results[j].lineColor+"; color: #"+this.defaults.schedules[i].results[j].lineColor+";")
+                lineSpan.setAttribute('data-line', this.defaults.schedules[i].results[j].lineColor);
+                lineSpan.innerHTML = this.defaults.schedules[i].results[j].lineCode;
                 lineCell.appendChild(lineSpan);
                 row.appendChild(lineCell);
     
                 var timeStampCell = document.createElement("td");
-                var timeStamp = this.departures[i].nextTrains[j].departureTime;
+                var timeStamp = this.defaults.schedules[i].results[j].departureTime;
                 timeStampCell.className = "headSign";
                 // TODO : regex on datetime 20210603T221600
                 timeStampCell.innerHTML = timeStamp.substring(timeStamp.indexOf('T')+1, timeStamp.indexOf('T')+3)
@@ -78,7 +106,7 @@ Module.register('MMM-Navitia', {
     
                 var headSignCell = document.createElement("td");
                 headSignCell.className = "headSign";
-                headSignCell.innerHTML = this.departures[i].nextTrains[j].headSign;
+                headSignCell.innerHTML = this.defaults.schedules[i].results[j].headSign;
                 row.appendChild(headSignCell);
             }
 
@@ -89,19 +117,19 @@ Module.register('MMM-Navitia', {
 	},
 
 
-
-
     loaded: function(callback) {
         Log.log(this.name + ' is loaded!');
         callback();
     },
 
+
     getStyles: function () {
 		return ["MMM-Navitia.css"];
 	},
 
+
     socketNotificationReceived: function(notification, payload) {
-        this.departures = payload;
+        this.defaults.schedules = payload;
         this.updateDom();
         Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
     }
--- node_helper.js
+++ node_helper.js
@@ -1,7 +1,7 @@
 var NodeHelper = require("node_helper");
 
 const Log = require("logger");
-const got = require("got");
+const fetch = require("node-fetch");
 const { Console } = require("console");
 
 module.exports = NodeHelper.create({
@@ -14,7 +14,7 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         switch(notification)
         {
-            case 'MMM-Navitia--INIT':
+            case 'MMM-Navitia--Departures':
                 this.getDepartures(payload);
                 break;
         }
@@ -24,73 +24,138 @@ module.exports = NodeHelper.create({
     //this.sendSocketNotification('SET_CONFIG', this.config);
 
 
-    getDepartures: function(config) {
+    getDepartures: async function(schedules) {
 
-        // config.departures.forEach((departure) => {
-        for (var indexConfig=0; indexConfig<config.departures.length; indexConfig++)
+        for (var index = 0; index < schedules.length; index++)
         {
-            var departure = config.departures[indexConfig];
-            var nextDepartures = [];
-
-            // TODO Try Catch on config settings
-
-            var url = config.navitiaURL 
+            var url = schedules[index].config.navitiaURL 
                 + "/stop_points/stop_point:"
-                + departure.departureStopPoint
+                + schedules[index].request.departureStopPoint
                 + "/lines/line:"
-                + departure.departureLine
+                + schedules[index].request.departureLine
                 + "/departures?direction_type="
-                + departure.direction
+                + schedules[index].request.direction
                 + "&key="
-                + config.navitiaApiKey;
+                + schedules[index].config.navitiaApiKey;
 
-            (async() => {
-                try {
-                    var navitiaResponse = await got(url);
-                    var navitiaJson = JSON.parse(navitiaResponse.body);
-                    var stopPoint = '';
-                    var nextTrains = [];
-                    
-                    // Maximum results depending on the limit
-                    for (let i = 0; i < navitiaJson.departures.length && nextTrains.length < config.limit; i++) {
-
-                        // Trains only
-                        if (navitiaJson.departures[i].display_informations.physical_mode == "Train")
-                        {
-                            var nextDeparture = 
-                            {
-                                "headSign":navitiaJson.departures[i].display_informations.headsign,
-                                "lineCode":navitiaJson.departures[i].display_informations.code,
-                                "lineColor":navitiaJson.departures[i].display_informations.color,
-                                "departureTime":navitiaJson.departures[i].stop_date_time.base_departure_date_time,
-                                "direction":navitiaJson.departures[i].route.direction.name
-                            }
-                            if (stopPoint == '')
-                            {
-                                stopPoint = navitiaJson.departures[i].stop_point.name;
-                            }
+            // API Call
+            try {
+                var navitiaResponse = await fetch(url);
+            }
+            catch(error) {
+                console.log (url + "\r\n" + error.message);
+            }
             
-                            nextTrains.push(nextDeparture);
+            // Loading JSON
+            try {
+                var data = await navitiaResponse.json();
+            }
+            catch(error) {
+                console.log (navitiaResponse + "\r\n" + error.message);
+            }
+
+            // Parsing
+            try {
+                var nextDepartures = [];
+
+                for (var i = 0; i < data.departures.length && nextDepartures.length < schedules[index].config.limit; i++) {
+
+                    // Trains only
+                    if (data.departures[i].display_informations.physical_mode == "Train")
+                    {
+                        var nextDeparture = 
+                        {
+                            headSign                : data.departures[i].display_informations.headsign,
+                            lineCode                : data.departures[i].display_informations.code,
+                            lineColor               : data.departures[i].display_informations.color,
+                            departureTime           : data.departures[i].stop_date_time.base_departure_date_time,
+                            direction               : data.departures[i].route.direction.name,
+                            departureStopPointName  : data.departures[i].stop_point.name
                         }
+
+                        nextDepartures.push(nextDeparture);
                     }
-
-                    nextDepartures.push({"departureStopPoint":stopPoint,"nextTrains":nextTrains})
-
-                    console.log("success");
-
-                    // config.nextDepartures[indexConfig].departureStopPoint = stopPoint;
-                    // config.nextDepartures[indexConfig].nextTrains = nextTrains;
-
-                    this.sendSocketNotification('MMM-Navitia-Departures', nextDepartures);
-
                 }
-                catch(error) {
-                    console.log(error.message);
-                }
-            })();
+                schedules[index].results = nextDepartures;
+                console.log("MMM-Navitia: Api parsing Departures successful");
+                this.sendSocketNotification('MMM-Navitia-Departures', schedules);
+            }
+            catch(error)
+            {
+                console.log(error.message);
+            }
 
-        };
+        }
     },
+
+
+
+    // getDepartures: function(config) {
+
+    //     // config.departures.forEach((departure) => {
+    //     for (var index=0; index<config.departures.length; index++)
+    //     {
+    //         // var departure = config.departures[index];
+    //         // var nextDepartures = [];
+
+    //         // TODO Try Catch on config settings
+
+    //         var url = config.navitiaURL 
+    //             + "/stop_points/stop_point:"
+    //             + config.departures[index].departureStopPoint
+    //             + "/lines/line:"
+    //             + config.departures[index].departureLine
+    //             + "/departures?direction_type="
+    //             + config.departures[index].direction
+    //             + "&key="
+    //             + config.navitiaApiKey;
+
+    //         (async() => {
+    //             try {
+    //                 var navitiaResponse     = await got(url);
+    //                 var navitiaJson         = JSON.parse(navitiaResponse.body);
+    //                 var stopPoint           = '';
+    //                 var nextTrains  = [];
+                    
+    //                 // Maximum results depending on the limit
+    //                 for (var i = 0; i < navitiaJson.departures.length && nextTrains.length < config.limit; i++) {
+
+    //                     // Trains only
+    //                     if (navitiaJson.departures[i].display_informations.physical_mode == "Train")
+    //                     {
+    //                         var nextDeparture = 
+    //                         {
+    //                             "headSign"      :navitiaJson.departures[i].display_informations.headsign,
+    //                             "lineCode"      :navitiaJson.departures[i].display_informations.code,
+    //                             "lineColor"     :navitiaJson.departures[i].display_informations.color,
+    //                             "departureTime" :navitiaJson.departures[i].stop_date_time.base_departure_date_time,
+    //                             "direction"     :navitiaJson.departures[i].route.direction.name
+    //                         }
+    //                         if (stopPoint == '')    stopPoint = navitiaJson.departures[i].stop_point.name;
+            
+    //                         nextTrains.push(nextDeparture);
+    //                     }
+    //                 }
+
+    //                 nextDepartures.push({"departureStopPoint":stopPoint,"nextTrains":nextTrains})
+
+    //                 console.log("success");
+
+    //                 // config.nextDepartures[indexConfig].departureStopPoint = stopPoint;
+    //                 // config.nextDepartures[indexConfig].nextTrains = nextTrains;
+
+    //                 // TODO
+    //                 // comment spécifier le bon departure au client
+    //                 this.sendSocketNotification('MMM-Navitia-Departures', nextDepartures);
+
+    //             }
+    //             catch(error) {
+    //                 console.log(error.message);
+    //             }
+    //         })();
+
+    //     };
+    // },
 
 
 });
